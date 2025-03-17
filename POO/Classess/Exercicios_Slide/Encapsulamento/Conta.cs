8547d422e27b8b7489aad3202114308dea9eea91
Fernando Barreto <152815498+Kalimbinha@users.noreply.github.com>
2025-03-17 20:01:38 -0300

    Melhoria na distribuição dos arquivos

---
+++ POO/Classess/Exercicios_Slide/Encapsulamento/Conta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta
+{
+    internal class Conta
+    {
+        private double saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+        }
+
+        public Conta(double saldoInicial)
+        {
+            Console.Clear();
+            Console.WriteLine("Sua conta foi instanciada");
+            saldo = saldoInicial;
+        }
+    }
+}
---
+++ POO/Classess/Exercicios_Slide/Encapsulamento/Exercicio_1.cs
@@ -0,0 +1,21 @@
+﻿//Exercício 1
+//• Implemente o software a partir do diagrama de classes especificado
+//• Considere que o usuário tem um saldo em sua conta, e dinheiro em sua
+//carteira
+//• O usuário não deve poder sacar mais dinheiro do que possui em sua conta, e
+//não deve poder depositar mais dinheiro do que possui em sua carteira
+
+using System;
+
+namespace Conta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta(200);
+            Console.WriteLine(conta.Saldo);
+            
+        }
+    }
+}
